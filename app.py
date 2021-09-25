9876f6beea1d84afe51cd22ec67d84acb3db5123
r2dev2 <ronak.badhe@gmail.com>
2021-09-25 15:48:38 -0700

    make email link to a preview and download of the article

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import io
 import os
 from functools import partial
 
+from aiohttp import ClientSession
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, StreamingResponse
 
+from auth import is_authenticated
+from core import article_name_to_file_name, get_article, get_html_article, save_article
 from models import Article
 from send import GMAIL
 
@@ -11,14 +17,56 @@ assert all(map(os.environ.get, ["SENDER_EMAIL", "SENDER_PASSWORD", "GUAPO_EMAIL"
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 def create_sender() -> None:
     email = GMAIL(os.environ["SENDER_EMAIL"], os.environ["SENDER_PASSWORD"])
     app.submit_article = partial(email.send_message, os.environ["GUAPO_EMAIL"])
 
 
+@app.on_event("startup")
+def create_http_session() -> None:
+    app.session = ClientSession()
+
+
+@app.on_event("shutdown")
+async def close_http_session() -> None:
+    await app.session.close()
+
+
+@app.get("/preview", response_class=HTMLResponse)
+def preview(article: int):
+    return HTMLResponse(get_html_article(article))
+
+
+@app.get("/download")
+def download(article: int):
+    article = get_article(article)
+    response = StreamingResponse(io.StringIO(article.body), media_type="text/markdown")
+    fname = article_name_to_file_name(article.title)
+    response.headers["Content-Disposition"] = f"attachment; filename={fname}"
+    return response
+
+
 @app.post("/article")
 def upload_article(article: Article):
+    print("Got article", article.body)
+    num = save_article(article)
     subject = f"Article creation/change request: {article.title}"
-    app.submit_article(subject, article.body)
+    body = f"""
+    Preview: https://submedit.r2dev2bb8.repl.co/preview?article={num}
+    Download: https://submedit.r2dev2bb8.repl.co/download?article={num}
+    """
+    app.submit_article(subject, body)
     return 200
+
+@app.get("/")
+def index():
+    return "Submedit api"
---
+++ core.py
@@ -0,0 +1,68 @@
+import os
+from contextlib import suppress
+from operator import attrgetter
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+from models import Article
+
+
+articles = Path(__file__).parent / "articles"
+
+
+def article_name_to_file_name(aname: str) -> str:
+    return aname.replace(" ", "_") + ".md"
+
+
+def save_article(article: Article) -> int:
+    amount_articles = len(os.listdir(articles))
+    with open(articles / f"{amount_articles}.json", "w+") as fout:
+        print(article.json(), file=fout)
+    return amount_articles
+
+
+def get_article(article_num: int) -> Article:
+    with suppress(FileNotFoundError):
+        with open(articles / f"{article_num}.json", "r") as fin:
+            return Article.parse_raw(fin.read())
+    return Article(title="Not found", body="may guapo luke guide you")
+
+
+def get_html_article(article_num: int) -> str:
+    article = get_article(article_num)
+    html_body = __remove_tag(__center_images(markdown(
+        article.body, extensions=["extra"])), "h1")
+    return f"""
+    <html>
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <title>{article.title} - ALSWiki</title>
+            <link rel="stylesheet" href="https://alswiki.github.io/index.css" />
+        </head>
+        <body>
+            <main class="md">
+                <div class="article">
+                    <h1>{article.title}</h1>
+                    {html_body}
+                </div>
+            </main>
+        </body>
+    </html>
+    """
+
+
+def __center_images(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    for p in map(attrgetter("parent"), soup.select("p > img")):
+        p["align"] = "center"
+    return str(soup)
+
+
+def __remove_tag(html: str, tag: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    for el in soup.select(tag):
+        el.replace_with("")
+    return str(soup)
